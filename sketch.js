373176d248189f737a5adfdc5d6b42d8afe9ea5b
sindhuja-v-jain <sindhuja.v.jain@gmail.com>
2020-11-19 23:15:36 +0530

    testing

--- sketch.js
+++ sketch.js
@@ -18,6 +18,7 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
 platform = new Ground(200, 200 , 100 , 100 )
+platform1 = new Ground(200, 200 , 100 , 100 )
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -53,7 +54,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
-
+platform1.display()
     box5.display();
     log4.display();
     log5.display();
